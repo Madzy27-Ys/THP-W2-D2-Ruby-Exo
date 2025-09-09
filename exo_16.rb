fd3c4b2d777e104ffa4ff95499a6f05e6e543f26
Madzy27-Ys <yanis.switaj27@gmail.com>
2025-09-09 11:51:32 +0200

    Exo 16

--- exo_16.rb
+++ exo_16.rb
@@ -2,7 +2,7 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 number = gets.chomp.to_i
 puts "Voici la pyramide :"
-for i in 0..number
-    print "#" * i
-    puts
+for i in 1..number
+    print " " * (number - i), "#" * i
+    puts 
 end
